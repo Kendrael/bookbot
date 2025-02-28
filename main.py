4a9feb93a42cc94bfd879b16dd8c4e68b2f9fb48
Kendrael <197850627+Kendrael@users.noreply.github.com>
2025-02-27 22:12:31 -0400

    rewrite main.py to return a string of a book

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("greetings boots")
+def get_book_text(path):
+    try:
+        with open(path) as f:
+            file_contents = f.read()
+        return file_contents
+    except FileNotFoundError:
+        print(f"Error: The file at '{path}' was not found.")
+        return ""
+    except IOError:
+        print(f"Error: An I/O error ocurred trying to read the file at '{path}'.")
+        return ""
+
+def main():
+    book_string = get_book_text("books/frankenstei.txt")
+    print(book_string)
+
+main()
